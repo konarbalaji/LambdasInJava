bc0ceab7f767680ce894def486442983e91e5c42
konarbalaji <konar.balaji@gmail.com>
2023-04-24 17:24:42 +0530

    Added new implementation of same logic using anonymous class

---
+++ src/main/java/ch02/ThreadAnonymous.java
@@ -0,0 +1,33 @@
+package ch02;
+
+public class ThreadAnonymous {
+    public static void main(String[] args) {
+
+        //With anonymous class
+        Thread t1 = new Thread(new Runnable(){
+
+            @Override
+            public void run() {
+                performLongRunningOperation();
+            }
+        });
+
+        t1.start();
+
+        Thread t2 = new Thread(new MyThread());
+        t2.start();
+    }
+
+
+    public static void performLongRunningOperation(){
+        System.out.println("This is a long running operation...");
+    }
+}
+
+class MyThread implements Runnable{
+
+    @Override
+    public void run() {
+        System.out.println("This is another long running operation...");
+    }
+}
