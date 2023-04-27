a497b2bc1f7bb3300d763f18a5f84f83ee3409c2
konarbalaji <konar.balaji@gmail.com>
2023-04-27 13:39:31 +0530

    Added test classes Supplier Functional interface

--- src/main/java/chp04/SupplierTest.java
+++ src/main/java/chp04/SupplierTest.java
@@ -26,7 +26,17 @@ public class SupplierTest {
 
         System.out.println("Time taken loop without autoboxing : " + (System.currentTimeMillis()-start1));
 
+        //Example Three - Get 2 coupons randomly
+        System.out.println("Two Coupons : ");
+        System.out.println(Coupons.getAvailableCoupons(2, () -> Coupons.getCoupons()));
 
-        //2nd
+        //2. Get total of 6 coupons randomly but budgets restricted to $500
+        System.out.println("Get 6 Coupons restricted to $500");
+        System.out.println(Coupons.getAvailableCoupons(6, () -> Coupons.getCouponsUpto500Max()));
+
+        System.out.println("Getting top valued coupon");
+        Supplier<String> getTopValuedCoupon = () -> Coupons.getTopValuedCoupon();
+        System.out.println(Coupons.getAvailableCoupons(10,getTopValuedCoupon));
+
     }
 }
