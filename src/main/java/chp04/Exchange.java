272ec5de8cb07493b87b690a02952ec0beb093aa
konarbalaji <konar.balaji@gmail.com>
2023-04-28 22:42:29 +0530

    Added more Classes demonstrating Function interface

---
+++ src/main/java/chp04/Exchange.java
@@ -0,0 +1,62 @@
+package chp04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.function.Predicate;
+
+public class Exchange {
+
+    //1.Get Symbol of stocks where volume is >= 1 million
+    public static List<String> getHighVolumeStocks(List<Trade> trades){
+
+        List<String> stocks = new ArrayList<>();
+
+        for(Trade t : trades){
+            if(t.getVolume() >= 1000000){
+                stocks.add(t.getStock().getSymbol());
+            }
+        }
+
+        return stocks;
+
+    }
+
+    //2.Get Symbol of Stock by type of Transactions
+    public static List<String> getStocksByTradeType(List<Trade> trades, TradeType tradeType){
+        List<String> stocks = new ArrayList<>();
+
+        for(Trade t : trades){
+            if(t.getTradeType()==tradeType){
+                stocks.add(t.getStock().getSymbol());
+            }
+        }
+
+        return stocks;
+    }
+
+    public static List<String> getStock(List<Trade> trades, Predicate<Trade> tradeFilter, Function<Trade, String> tradeTransformer){
+
+        Objects.requireNonNull(tradeFilter);
+        Objects.requireNonNull(tradeTransformer);
+
+        List<String> stocks = new ArrayList<>();
+
+        for(Trade t : trades){
+            if(tradeFilter.test(t)){
+                stocks.add(tradeTransformer.apply(t));
+            }
+        }
+
+        return stocks;
+    }
+
+    public static Function<Trade,String> getStockSymbolWithVolume(){
+        return (trade) -> trade.getStock().getSymbol() + "-" + trade.getVolume();
+    }
+
+    public static Function<Trade,String> getStockNameSymbolWithVolume(){
+        return (trade) -> trade.getStock().getName() + "-" + trade.getStock().getSymbol() + "-" + trade.getVolume();
+    }
+}
---
+++ src/main/java/chp04/TradeType.java
@@ -0,0 +1,6 @@
+package chp04;
+
+public enum TradeType {
+    BUY,
+    SELL;
+}
