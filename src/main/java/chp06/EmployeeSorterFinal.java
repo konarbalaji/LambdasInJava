82890b95c2fdf65f53d991632b68dfbf788baf62
konarbalaji <konar.balaji@gmail.com>
2023-04-29 21:41:14 +0530

    Implemented enhanced comparator technique

--- src/main/java/chp06/EmployeeSorterFinal.java
+++ src/main/java/chp06/EmployeeSorterFinal.java
@@ -4,18 +4,23 @@ import chp06.employee.Employee;
 
 import java.util.Comparator;
 
+import static java.util.Comparator.comparing;
+import static java.util.Comparator.comparingInt;
+
 public class EmployeeSorterFinal {
 
     //keyextractor - field that is used to sort objects.
     //comparing - pass me the field that i can use to compare objects.
     public static final Comparator<Employee> BY_NAME_ALPHA =
-            Comparator.comparing((Employee e) -> e.getName());
+            comparing((Employee e) -> e.getName());
+
+    //Comparator for sorting employee by experience
+    public static final Comparator<Employee> BY_EXPERIENCE =
+            comparingInt((Employee e) -> e.getExperience());
 
     public static final Comparator<Employee> BY_ASC_EXP_THEN_NAME =
-            Comparator.comparing((Employee e) -> e.getExperience()).
-                    thenComparing(BY_NAME_ALPHA);
+            BY_EXPERIENCE.thenComparing(BY_NAME_ALPHA);
 
     public static final Comparator<Employee> BY_DESC_EXP_THEN_NAME =
-            Comparator.comparing((Employee e) -> e.getExperience()).reversed()
-                    .thenComparing(BY_NAME_ALPHA);
+            BY_EXPERIENCE.reversed().thenComparing(BY_NAME_ALPHA);
 }
