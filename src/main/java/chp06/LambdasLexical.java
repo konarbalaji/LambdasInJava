1477090393f2d7d1aaa465f76e3b8f88a854430b
konarbalaji <konar.balaji@gmail.com>
2023-04-29 18:10:21 +0530

    Added Classes demonstrating method Hanlde . Added classes to test variable life scope in lambda vs anonymous classes

---
+++ src/main/java/chp06/LambdasLexical.java
@@ -0,0 +1,33 @@
+package chp06;
+
+public class LambdasLexical {
+
+    int i=10;
+
+    public void inner(){
+
+        int i =30;
+
+        Runnable r = () -> {
+            System.out.println(this.getClass().getName());
+            System.out.println("Accessing i directly inside lambda : " + i);
+            System.out.println("Accessing i using 'this' inside lambda : " + this.i);
+            System.out.println("This call is from inside lambda : " + this);
+            System.out.println(toString());
+        };
+
+        Thread t = new Thread(r);
+        t.start();
+
+    }
+
+    @Override
+    public String toString() {
+        return "LambdasLexical{ i=" + i + '}';
+    }
+
+    public static void main(String[] args) {
+        LambdasLexical lex = new LambdasLexical();
+        lex.inner();
+    }
+}
