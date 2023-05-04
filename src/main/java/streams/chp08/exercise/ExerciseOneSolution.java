b6928dbeef58f4abbdeb5801b560b0be86dbe4df
konarbalaji <konar.balaji@gmail.com>
2023-05-04 12:12:42 +0530

    Filter based on list & test mapping

--- src/main/java/streams/chp08/exercise/ExerciseOneSolution.java
+++ src/main/java/streams/chp08/exercise/ExerciseOneSolution.java
@@ -1,4 +1,23 @@
 package streams.chp08.exercise;
 
+import java.util.List;
+import java.util.Optional;
+import java.util.Set;
+
 public class ExerciseOneSolution {
+
+    public static Set<String> names = Set.of("software","programmer","i","an","a","java","the");
+    public static Set<String> articles = Set.of("a","an","the");
+
+    public static void main(String[] args) {
+
+        String art = names.stream()
+                .filter(a -> articles.contains(a))
+                .map(String::toUpperCase)
+                .findFirst()
+                .orElse("No Articles found");
+
+        System.out.println(art);
+
+    }
 }
---
+++ src/main/java/streams/chp08/exercise/ExerciseTwoSolution.java
@@ -0,0 +1,7 @@
+package streams.chp08.exercise;
+
+public class ExerciseTwoSolution {
+    public static void main(String[] args) {
+
+    }
+}
