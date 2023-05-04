6afb87f02bfae01a4aff5c82d3f387d483e18c45
konarbalaji <konar.balaji@gmail.com>
2023-05-04 12:32:28 +0530

    Filter based on list & test mapping

--- src/main/java/streams/chp08/exercise/ExerciseTwoSolution.java
+++ src/main/java/streams/chp08/exercise/ExerciseTwoSolution.java
@@ -1,7 +1,43 @@
 package streams.chp08.exercise;
 
+import lambdas.chp04.RemoveIfTest;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+
 public class ExerciseTwoSolution {
+
+    public static List<Movie> movies = MovieUtil.getMovieData();
+
     public static void main(String[] args) {
 
+
+        //Exercise 2 - Find first rocky movie
+        findFirstRockieMovie();
+
+        //Exercise 3  -are there any movies starting with Will Smith in 2020
+        willSmithMovie2020();
     }
+
+    private static void willSmithMovie2020() {
+        boolean willIn2020 = movies.stream()
+                                    //.filter(m -> m.getCast().contains("Will Smith"))
+                                    .anyMatch(m -> ((m.getDateOfRelease().getYear() == 2020) && (m.getCast().contains("Will Smith"))));
+
+        System.out.println("willIn2020 : " + willIn2020);
+    }
+
+    private static void findFirstRockieMovie() {
+
+        Comparator<Movie> byReleaseDate = Comparator.comparing(Movie::getDateOfRelease);
+
+        Optional<Movie> res = movies.stream()
+                                    .filter(m -> m.getName().contains("Rocky"))
+                                    .sorted(byReleaseDate)
+                                    .findFirst();
+
+        System.out.println(res.orElseThrow());
+    }
+
 }
---
+++ src/main/java/streams/chp08/exercise/Genre.java
@@ -0,0 +1,11 @@
+package streams.chp08.exercise;
+
+public enum Genre {
+	
+		HORROR,
+		COMEDY,
+		ACTION,
+		SUPERHERO,
+		CRIME,
+		MYSTERY;
+}
