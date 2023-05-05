014ccea807e81b231796d863ddcecfba5c8ee6b3
konarbalaji <konar.balaji@gmail.com>
2023-05-05 15:25:59 +0530

    Exploring APIs in Collectors

--- src/main/java/streams/chp10/FootballTeamTest.java
+++ src/main/java/streams/chp10/FootballTeamTest.java
@@ -50,9 +50,54 @@ public class FootballTeamTest {
         Map<League, IntSummaryStatistics> stats = IntSummaryStatistics(data);
         System.out.println(stats);
 
-        //9
+        //9. comma separated team names
+        System.out.println("\n9. comma separated team names");
+        String commaSepNames = commaSeparatesNamesOfTeam(data);
+        System.out.println(commaSepNames);
 
+        /*10.Fetch names of the teams,separate them by comma but group them by league.
 
+		{ LA_LIGA=Real Sociedad,Villareal,Atletico Madrid,
+		  BUNDESLIGA=Bayern,RB Leipzig,Dortmund,
+		  PREMIER_LEAGUE=Leicester City,Tottenham,Liverpool,
+		  CHAMPIONS_LEAGUE=Bayern,Atletico Madrid,Liverpool
+		}
+		*/
+
+        System.out.println("\n10.Fetch names of the teams,separate them by comma but group them by league.");
+        Map<League, String> csTeamNamesByLeague = commaSeparatesNamesOfTeamByLeague(data);
+        System.out.println(csTeamNamesByLeague);
+
+        //11.multi level partition.
+        System.out.println("\n11.multi level partition.");
+        Map<Boolean, Map<League, List<FootballTeam>>> part = multiLevelPartitionCL(data);
+        System.out.println(part);
+
+    }
+
+    private static Map<Boolean, Map<League, List<FootballTeam>>> multiLevelPartitionCL(List<FootballTeam> data) {
+        return data.stream()
+                    .collect(Collectors.partitioningBy(
+                            team -> isTeamInChampionsLeague(team),
+                            Collectors.groupingBy(FootballTeam::getLeague)
+                    ));
+
+    }
+
+    private static Map<League, String> commaSeparatesNamesOfTeamByLeague(List<FootballTeam> data) {
+
+        return data.stream()
+                    .collect(Collectors.groupingBy(
+                            FootballTeam::getLeague,
+                            Collectors.mapping(team -> team.getName(), Collectors.joining(","))));
+
+    }
+
+    private static String commaSeparatesNamesOfTeam(List<FootballTeam> data) {
+
+        return data.stream()
+                    .map(team -> team.getName())
+                    .collect(Collectors.joining(",","[","]"));
 
     }
 
