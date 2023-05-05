2800e437f01a6d4d644915683b0d2d1d739d7228
konarbalaji <konar.balaji@gmail.com>
2023-05-05 15:08:58 +0530

    Exploring APIs in Collectors

--- src/main/java/streams/chp10/FootballTeamTest.java
+++ src/main/java/streams/chp10/FootballTeamTest.java
@@ -1,9 +1,7 @@
 package streams.chp10;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class FootballTeamTest {
 
@@ -18,6 +16,91 @@ public class FootballTeamTest {
         //2. grouping imperative data
         imperativeGrouping(data);
 
+        //3.Counting total teams in each league
+        Map<League, Long> totalTeamsByLeague = totalTeamsInEachLeague(data);
+        System.out.println("\n3. Total teams in each League : " + totalTeamsByLeague);
+
+        //4. Total goals in each league
+        Map<League, Integer> goalsByLeague = totalGoalsInEachLeague(data);
+        System.out.println("\n4. Total goals in each league : " + goalsByLeague);
+
+        //5. partition teams by the ones i8n the champions league and the ones that are not
+        System.out.println("\n5. partition teams by the ones in the champions league and the ones that are not");
+        Map<Boolean, List<FootballTeam>> partitionedByChampionsLeague = partitionByCL(data);
+        System.out.println(partitionedByChampionsLeague);
+
+        //6. Grouping team by attacking, defensive
+        System.out.println("\n6. Grouping team by attacking, defensive");
+        Map<String, List<FootballTeam>> byStyle = groupByAttackDefense(data);
+        System.out.println(byStyle);
+
+        /*7.Grouping by attacking/defensive but league wise
+
+        LA_LIGA={ATTACKING=[Real Sociedad], DEFENSIVE=[Villareal, Atletico Madrid]},
+        BUNDESLIGA={ATTACKING=[Bayern], DEFENSIVE=[RB Leipzig, Dortmund]},
+        PREMIER_LEAGUE=[Leicester City, Tottenham, Liverpool],
+        CHAMPIONS_LEAGUE=[Bayern, Atletico Madrid, Liverpool]}*/
+
+        System.out.println("7.Grouping by attacking/defensive but league wise");
+        Map<League, Map<String, List<FootballTeam>>> byStyleInEachLeague = groupByAttackDefenseLeague(data);
+        System.out.println(byStyleInEachLeague);
+
+        //8. IntSummaryStatistics in each league
+        System.out.println("\n8. IntSummaryStatistics in each league");
+        Map<League, IntSummaryStatistics> stats = IntSummaryStatistics(data);
+        System.out.println(stats);
+
+        //9
+
+
+
+    }
+
+    private static Map<League, IntSummaryStatistics> IntSummaryStatistics(List<FootballTeam> data) {
+        return data.stream()
+                    .collect(Collectors.groupingBy(FootballTeam::getLeague,
+                            Collectors.summarizingInt(FootballTeam::getGoalsFor)));
+    }
+
+    private static Map<League, Map<String, List<FootballTeam>>> groupByAttackDefenseLeague(List<FootballTeam> data) {
+        return data.stream()
+                .collect(Collectors.groupingBy(FootballTeam::getLeague,
+                        Collectors.groupingBy(team -> getStyleOfTeam(team))));
+    }
+
+    private static Map<String, List<FootballTeam>> groupByAttackDefense(List<FootballTeam> data) {
+        return data.stream()
+                    .collect(Collectors.groupingBy(team -> getStyleOfTeam(team)));
+    }
+
+    private static String getStyleOfTeam(FootballTeam team) {
+        if(team.getGoalsFor() > 20)
+            return "ATTACKING";
+        return "DEFENSIVE";
+    }
+
+
+    private static Map<Boolean, List<FootballTeam>> partitionByCL(List<FootballTeam> data) {
+
+        return data.stream()
+                .collect(Collectors.partitioningBy(team -> isTeamInChampionsLeague(team)));
+
+    }
+
+    public static boolean isTeamInChampionsLeague(FootballTeam team){
+        return team.getLeague().equals(League.CHAMPIONS_LEAGUE);
+    }
+
+    private static Map<League, Integer> totalGoalsInEachLeague(List<FootballTeam> data) {
+        return data.stream()
+                .collect(Collectors.groupingBy(FootballTeam::getLeague,
+                        Collectors.summingInt(FootballTeam::getGoalsFor)));
+    }
+
+    private static Map<League, Long> totalTeamsInEachLeague(List<FootballTeam> data) {
+        return data.stream()
+                    .collect(Collectors.groupingBy(FootballTeam::getLeague,
+                            Collectors.counting()));
     }
 
     private static void imperativeGrouping(List<FootballTeam> data) {
@@ -35,7 +118,7 @@ public class FootballTeamTest {
             group.get(league).add(team);
         }
 
-        System.out.println("\n Imperative Style : " + group);
+        System.out.println("\n2. Imperative Style : " + group);
 
       /*  group.forEach((k,v) -> {
             System.out.println("Key : " + k);
@@ -48,7 +131,7 @@ public class FootballTeamTest {
             List<FootballTeam> teamList = group.computeIfAbsent(league, k -> new ArrayList<>());
             teamList.add(team);
         }
-        System.out.println("\n Imperative style two : " + group);
+        System.out.println("\nImperative style two : " + group);
 
     }
 }
